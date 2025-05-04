6a15f679bde521361139005a800cb7836340f14b
Rafifanwar <117049583+Rafifanwar@users.noreply.github.com>
2025-05-04 23:35:53 +0800

    Update builld_files.sh

--- builld_files.sh
+++ builld_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
+
+# Make build_files.sh executable
+chmod +x build_files.sh
